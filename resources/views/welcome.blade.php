f38f4fec22265bbd8c44023d2e54ef56ba4b93e7
Antoine Greuzard <contact@antoinegreuzard.eu>
2018-02-28 21:08:34 +0100

    ça marche pas help

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,97 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Fullcalendar and Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    {!! Html::style('css/app.scss') !!}
+    {!! Html::style('vendor/antoine/fullcalendar/fullcalendar.min.css') !!}
+</head>
+<body>
+<div class='container'>
+    <div id='calendar'></div>
+</div>
+</body>
+{!! Html::script('js/app.js') !!}
+{!! Html::script('cendor/antoine/fullcalendar/lib/moment.min.js') !!}
+{!! Html::script('cendor/antoine/fullcalendar/lib/jquery.min.js') !!}
+{!! Html::script('cendor/antoine/fullcalendar/fullcalendar.min.js') !!}
+<script>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    $(document).ready(function() {
 
-            .full-height {
-                height: 100vh;
-            }
+        $('#calendar').fullCalendar({
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay,listWeek'
+            },
+            defaultDate: '2018-02-12',
+            navLinks: true, // can click day/week names to navigate views
+            editable: true,
+            eventLimit: true, // allow "more" link when too many events
+            events: [
+                {
+                    title: 'All Day Event',
+                    start: '2018-02-01',
+                },
+                {
+                    title: 'Long Event',
+                    start: '2018-02-07',
+                    end: '2018-02-10'
+                },
+                {
+                    id: 999,
+                    title: 'Repeating Event',
+                    start: '2018-02-09T16:00:00'
+                },
+                {
+                    id: 999,
+                    title: 'Repeating Event',
+                    start: '2018-02-16T16:00:00'
+                },
+                {
+                    title: 'Conference',
+                    start: '2018-02-11',
+                    end: '2018-02-13'
+                },
+                {
+                    title: 'Meeting',
+                    start: '2018-02-12T10:30:00',
+                    end: '2018-02-12T12:30:00'
+                },
+                {
+                    title: 'Lunch',
+                    start: '2018-02-12T12:00:00'
+                },
+                {
+                    title: 'Meeting',
+                    start: '2018-02-12T14:30:00'
+                },
+                {
+                    title: 'Happy Hour',
+                    start: '2018-02-12T17:30:00'
+                },
+                {
+                    title: 'Dinner',
+                    start: '2018-02-12T20:00:00'
+                },
+                {
+                    title: 'Birthday Party',
+                    start: '2018-02-13T07:00:00'
+                },
+                {
+                    title: 'Click for Google',
+                    url: 'http://google.com/',
+                    start: '2018-02-28'
+                }
+            ]
+        });
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    });
 
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+</script>
 </html>
