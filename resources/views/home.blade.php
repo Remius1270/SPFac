713ca331ba726d985145d3f3cba5b032ed360305
Antoine Greuzard <antoin.greuzard@gmail.com>
2018-03-03 22:26:10 +0100

    affichage calendier + event

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,60 +1,66 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-10">
-      <div class="card">
-        <div class="card-header">Vos rendez-vous</div>
-        <div class="card-body">
-          <table class="table table-responsive-sm table-hover table-striped table-outline mb-0">
-            <thead class="thead-light">
-              <tr>
-                <th>Date</th>
-                <th>Nom</th>
-                <th>Titre</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              @if(count($rdvs)>0)
-              @foreach($rdvs as $rdv)
-              
-              <tr>
-                <td id="date_{{$rdv->id}}">{{date('d/m/Y à H\hi', strtotime($rdv->timestamp))}}</td>
-                <td id="name_{{$rdv->id}}">{{$rdv->name}}</td>
-                <td id="title_{{$rdv->id}}">{{$rdv->title}}</td>
-                <td>
-                    <button id="{{$rdv->id}}" class="btn btn-warning btn-edit" data-toggle="modal" data-target="#editModal">Editer</button>
-                    <a href="/deleteRdv/{{ $rdv->id }}" class="btn btn-danger">Supprimer</a>
-                </td>
-              </tr>
-              @endforeach
-              @else
-              <tr><td>Pas de rendez vous prévu.</td><tr/>
-              @endif
-            </tbody>
-          </table>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header">Vos rendez-vous</div>
+                    <div class="card-body">
+                        <table class="table table-responsive-sm table-hover table-striped table-outline mb-0">
+                            <thead class="thead-light">
+                            <tr>
+                                <th>Date</th>
+                                <th>Nom</th>
+                                <th>Titre</th>
+                                <th>Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @if(count($rdvs)>0)
+                                @foreach($rdvs as $rdv)
 
-          <button type="button" class="btn btn-primary btn-addrdv" data-toggle="modal" data-target="#myModal">Ajouter un Rendez-vous</button>
+                                    <tr>
+                                        <td id="date_{{$rdv->id}}">{{date('d/m/Y à H\hi', strtotime($rdv->timestamp))}}</td>
+                                        <td id="name_{{$rdv->id}}">{{$rdv->name}}</td>
+                                        <td id="title_{{$rdv->id}}">{{$rdv->title}}</td>
+                                        <td>
+                                            <button id="{{$rdv->id}}" class="btn btn-warning btn-edit"
+                                                    data-toggle="modal" data-target="#editModal">Editer
+                                            </button>
+                                            <a href="/deleteRdv/{{ $rdv->id }}" class="btn btn-danger">Supprimer</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @else
+                                <tr>
+                                    <td>Pas de rendez vous prévu.</td>
+                                <tr/>
+                            @endif
+                            </tbody>
+                        </table>
 
+                        <button type="button" class="btn btn-primary btn-addrdv" data-toggle="modal"
+                                data-target="#myModal">Ajouter un Rendez-vous
+                        </button>
+
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-</div>
 
-<!-- Modal add -->
-<div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-    <form method="POST" action="{{ route('addRdv') }}">
-        <!-- Modal content-->
-        <div class="modal-content">
-          <div class="modal-header">
-            <h4 class="modal-title">Ajouter un Rendez-vous</h4>
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-          </div>
-          <div class="modal-body">
+    <!-- Modal add -->
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <form method="POST" action="{{ route('addRdv') }}">
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Ajouter un Rendez-vous</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
 
                         @csrf
 
@@ -62,7 +68,9 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">Nom</label>
 
                             <div class="col-md-6">
-                                <input id="first_name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="first_name" type="text"
+                                       class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name"
+                                       value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
                                     <span class="invalid-feedback">
@@ -77,7 +85,9 @@
                             <label for="title" class="col-md-4 col-form-label text-md-right">Titre</label>
 
                             <div class="col-md-6">
-                                <input id="title" type="text" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" name="title" value="{{ old('title') }}" required>
+                                <input id="title" type="text"
+                                       class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" name="title"
+                                       value="{{ old('title') }}" required>
 
                                 @if ($errors->has('title'))
                                     <span class="invalid-feedback">
@@ -86,32 +96,31 @@
                                 @endif
                             </div>
                         </div>
-                     
+
 
                         <!-- mettre qqch pour pick la date et l'heure -->
 
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-            <button type="submit" class="btn btn-primary">Ajouter</button>
-          </div>
-       </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                        <button type="submit" class="btn btn-primary">Ajouter</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 
-  </div>
-</div>
-
-<!-- Modal edit -->
-<div id="editModal" class="modal fade modal-warning" role="dialog">
-  <div class="modal-dialog">
-    <form method="POST" action="">
-        <!-- Modal content-->
-        <div class="modal-content">
-          <div class="modal-header">
-            <h4 class="modal-title">Modifier un Rendez-vous</h4>
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-          </div>
-          <div class="modal-body">
+    <!-- Modal edit -->
+    <div id="editModal" class="modal fade modal-warning" role="dialog">
+        <div class="modal-dialog">
+            <form method="POST" action="">
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Modifier un Rendez-vous</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
 
                         @csrf
 
@@ -119,7 +128,9 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">Nom</label>
 
                             <div class="col-md-6">
-                                <input id="first_name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="first_name" type="text"
+                                       class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name"
+                                       value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
                                     <span class="invalid-feedback">
@@ -134,7 +145,9 @@
                             <label for="title" class="col-md-4 col-form-label text-md-right">Titre</label>
 
                             <div class="col-md-6">
-                                <input id="title" type="text" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" name="title" value="{{ old('title') }}" required>
+                                <input id="title" type="text"
+                                       class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" name="title"
+                                       value="{{ old('title') }}" required>
 
                                 @if ($errors->has('title'))
                                     <span class="invalid-feedback">
@@ -143,18 +156,21 @@
                                 @endif
                             </div>
                         </div>
-                     
+
 
                         <!-- mettre qqch pour pick la date et l'heure -->
 
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-            <button type="submit" class="btn btn-warning">Modifier</button>
-          </div>
-       </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                        <button type="submit" class="btn btn-warning">Modifier</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
+@endsection
 
-  </div>
-</div>
+@section('calendar')
+    <div id='calendar'></div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,35 +12,57 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.8.2/fullcalendar.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.8.2/fullcalendar.print.css" rel="stylesheet">
+    <link href="{{ asset('fullcalendar/fullcalendar.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('fullcalendar/fullcalendar.print.min.css') }}" rel='stylesheet' media='print'/>
+
+
+<!--<link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('bootstrap-datetimepicker/css/bootstrap-material-datetimepicker.css') }}" rel="stylesheet">
+    <link href="{{ asset('bootstrap-colorpicker/css/bootstrap-colorpicker.min.css') }}" rel="stylesheet"> -->
+
+    <style>
+        #calendar {
+            max-width: 900px;
+            margin: 0 auto;
+        }
+
+        .fc-view-container {
+            background: white;
+        }
+
+        body {
+            margin-bottom: 70px
+        }
+    </style>
 </head>
 <body>
-    <div id="app">
-        <nav class="app-header navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Rendez-Vous
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<div id="app">
+    <nav class="app-header navbar navbar-expand-md navbar-light navbar-laravel">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                Rendez-Vous
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
 
-                    </ul>
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                        <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
                         @else
                             <li class="nav-item">
-                                <a id="navbarDropdown" class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <a id="navbarDropdown" class="nav-link" href="#" role="button" data-toggle="dropdown"
+                                   aria-haspopup="true" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
@@ -51,26 +73,67 @@
                                         Se déconnecter
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                          style="display: none;">
                                         @csrf
                                     </form>
                                 </div>
                             </li>
-                        @endguest
-                    </ul>
-                </div>
+                            @endguest
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+    <main class="py-4">
+        @yield('content')
+    </main>
+</div>
 
-    <!-- Scripts -->
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ asset('js/calendar.js') }}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.8.2/fullcalendar.min.js" ></script>
+@yield('calendar')
+
+<!-- Scripts -->
+<!--<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script> -->
+<script src="{{ asset('js/app.js') }}"></script>
+
+
+<script src="{{ asset('fullcalendar/lib/moment.min.js') }}"></script>
+<script src="{{ asset('jquery.min.js') }}"></script>
+<script src="{{ asset('fullcalendar/fullcalendar.min.js') }}"></script>
+
+<!--<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>-->
+<!--<script src="{{ asset('bootstrap-datetimepicker/js/bootstrap-material-datetimepicker.js') }}"></script>
+    <script src="{{ asset('bootstrap-colorpicker/js/bootstrap-colorpicker.min.js') }}"></script>-->
+
+<script>
+    $(document).ready(function () {
+
+        $('#calendar').fullCalendar({
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,basicWeek,basicDay'
+            },
+            defaultDate: '{{ date('Y-m-d') }}',
+            navLinks: true, // can click day/week names to navigate views
+            editable: false,
+            eventLimit: true, // allow "more" link when too many events
+
+            events: [
+                    @if(count($rdvs)>0)
+                    @foreach($rdvs as $rdv)
+                {
+                    title: "{{$rdv->title}}",
+                    start: "{{(substr($rdv->timestamp, 0, 10)) }}"
+                },
+
+                    @endforeach
+                    @endif
+            ]
+        });
+
+    });
+
+</script>
 </body>
 </html>
